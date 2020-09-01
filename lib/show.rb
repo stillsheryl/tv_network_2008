4de474a887b2c74b507bc9a410848daef8823e0f
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-01 09:26:29 -0600

    Add total_salary method

--- lib/show.rb
+++ lib/show.rb
@@ -4,6 +4,13 @@ class Show
   def initialize(name, creator, characters)
     @name = name
     @creator = creator
-    @characters = []
+    @characters = characters
   end
+
+  def total_salary
+    @characters.sum do |character|
+      character.salary
+    end
+  end
+
 end
