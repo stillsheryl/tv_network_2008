2883710b060ba28ef05741980ac35f6af00f62cf
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-01 09:17:42 -0600

    Add test it can have name and creator attributes

--- test/show_test.rb
+++ test/show_test.rb
@@ -6,9 +6,20 @@ require './lib/show'
 class ShowTest < Minitest::Test
 
   def test_it_exists
-    show = Show.new
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
 
-    assert_instance_of Show, show
+    assert_instance_of Show, knight_rider
+  end
+
+  def test_it_has_attributes
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal "Knight Rider", knight_rider.name
+    assert_equal "Glen Larson", knight_rider.creator
   end
 
 end
