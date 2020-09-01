df5b96a6551219e31af2a6d3ea0a427afee769cd
Sheryl Stillman <sheryl.stillman@gmail.com>
2020-09-01 09:27:45 -0600

    Add test can calculate highest paid actor

--- test/show_test.rb
+++ test/show_test.rb
@@ -31,4 +31,12 @@ class ShowTest < Minitest::Test
     assert_equal 2600000, knight_rider.total_salary
   end
 
+  def test_has_highest__paid_actor
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    michael_knight = Character.new({name: "Michael Knight", actor: "David Hasselhoff", salary: 1_600_000})
+    knight_rider = Show.new("Knight Rider", "Glen Larson", [michael_knight, kitt])
+
+    assert_equal "David Hasselhoff", knight_rider.highest_paid_actor
+  end
+
 end
